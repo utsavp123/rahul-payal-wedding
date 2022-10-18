38d843a6f7f7886dc9166b1232fc2ed20366a5d0
utsavp <utsavparmar72@gmail.com>
2022-10-19 01:35:17 +0530

    heelo

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2022, 11, 25, 10)}); // year, month, date, hour
+        });
+});		
+
