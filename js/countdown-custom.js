91a92d41fec78e2f23bbd576b124f57c37e132eb
utsavp <utsavparmar72@gmail.com>
2022-10-20 23:27:35 +0530

    sdf

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function() {
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2022, 10,24 , 08)}); // year, month, date, hour
+            jQuery('#defaultCountdown').countdown({until: new Date(2022, 10 , 24 , 6.5)}); // year, month, date, hour
         });
 });		
 jQuery(document).ready(function() {
         jQuery(function () {
-            jQuery('#defaultCountdownmarr').countdown({until: new Date(2022, 10,25 , 10.2)}); // year, month, date, hour
+            jQuery('#defaultCountdownmarr').countdown({until: new Date(2022, 10, 25 , 10)}); // year, month, date, hour
         });
 });		
 
